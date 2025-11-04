72f6943ef8acadb36281fa77d80e88434786bccb
bennyw <409322456@qq.com>
2025-11-04 16:42:52 +0800

    Update TZPhotoPickerController.h
    实现调起相册直接进入拍摄界面

--- TZImagePickerController/TZImagePickerController/TZPhotoPickerController.h
+++ TZImagePickerController/TZImagePickerController/TZPhotoPickerController.h
@@ -14,6 +14,8 @@
 @property (nonatomic, assign) BOOL isFirstAppear;
 @property (nonatomic, assign) NSInteger columnNumber;
 @property (nonatomic, strong) TZAlbumModel *model;
+
+@property (nonatomic, assign) BOOL showCameraFirst;
 @end
 
 
